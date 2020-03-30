def7b1e89306538846fac625819bb12a61eb3813
Andy Wigley <anwigley@microsoft.com>
2020-03-30 11:18:40 +0100

    Added parameters

--- .github/actions/setsecret-action/setsecret.ts
+++ .github/actions/setsecret-action/setsecret.ts
@@ -17,30 +17,29 @@ const run = async (): Promise<void> => {
         owner,
         repo,
     })
-    console.log(`PublicKey: ${getPublicKeyResponse.data.key}`)
+    const publicKey = getPublicKeyResponse.data.key
+    console.log(`PublicKey: ${publicKey}`)
 
+    // Get the keys and values to work on
+    let data: Params[] = JSON.parse(core.getInput('data'));
 
-    // Encrypt the value
-    const value = core.getInput('variable-name');
-    // Convert the message and key to Uint8Array's (Buffer implements that interface)
-    const messageBytes = Buffer.from(value);
-    const keyBytes = Buffer.from(getPublicKeyResponse.data.key, 'base64');
-    // Encrypt using LibSodium.
-    const encryptedBytes = sodium.seal(messageBytes, keyBytes);
-    // Base64 the encrypted secret
-    const encrypted = Buffer.from(encryptedBytes).toString('base64');
-    console.log(`Encrypted value (Base64 encoded): ${encrypted}`);
+    data.forEach(async entry => {
+      console.log(`Saving Secret name ${entry.name}`);
+      // Convert the message and key to Uint8Array's (Buffer implements that interface)
+      const encrypted = encryptValue(entry.value, publicKey)
 
-    // Create or update a secret for the repository
-    // https://octokit.github.io/rest.js/v17#actions-create-or-update-secret-for-repo
-    await octokit.actions.createOrUpdateSecretForRepo({
-      owner,
-      repo,
-      name: 'HelloWorld',
-      encrypted_value: encrypted,
-      key_id: getPublicKeyResponse.data.key_id
-    })
-    console.log(`Saved ${value} in Secrets`)
+      // Create or update a secret for the repository
+      // https://octokit.github.io/rest.js/v17#actions-create-or-update-secret-for-repo
+      await octokit.actions.createOrUpdateSecretForRepo({
+        owner,
+        repo,
+        name: 'HelloWorld',
+        encrypted_value: encrypted,
+        key_id: getPublicKeyResponse.data.key_id
+      })
+      console.log(`Saved ${entry.name} in Secrets`)
+    });
+
   } catch (error) {
     console.error(error.message)
     core.setFailed(`SetSecret-action failure: ${error}`)
@@ -49,4 +48,20 @@ const run = async (): Promise<void> => {
 
 run()
 
+interface Params {
+  name: string;
+  value: string;
+}
+
 export default run
+
+function encryptValue(value: string, publicKey: string) {
+  const messageBytes = Buffer.from(value)
+  const keyBytes = Buffer.from(publicKey, 'base64')
+  // Encrypt using LibSodium.
+  const encryptedBytes = sodium.seal(messageBytes, keyBytes)
+  // Base64 the encrypted secret
+  const encrypted = Buffer.from(encryptedBytes).toString('base64')
+  console.log(`Encrypted value (Base64 encoded): ${encrypted}`)
+  return encrypted
+}
